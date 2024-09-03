87b3979e48ee21a7cb2e8bef3e7aa4221eb62205
GouravSoni9068 <gouravsoni9068@gmail.com>
2024-09-04 00:33:20 +0530

    variable

---
+++ variable.r
@@ -0,0 +1,24 @@
+num1=3
+print(num1)
+print(class(num1))
+
+num2=3.14
+print(num2)
+print(class(num1))
+
+
+char="Hello mr"
+print(char)
+print(class(char))
+
+log1=TRUE
+print(log1)
+print(class(log1))
+log2=FALSE
+print(log2)
+print(class(log2))
+
+
+com1=4+10i
+print(com1)
+print(class(com1))
