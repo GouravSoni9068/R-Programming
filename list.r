be5eeb4aa05278576d8426c4fd4ad69d95d496d4
GouravSoni9068 <gouravsoni9068@gmail.com>
2024-09-04 01:17:23 +0530

    list

---
+++ list.r
@@ -0,0 +1,17 @@
+# list1<-list(1,"Hello",T)
+# print(list1)
+# print(class(list1[[1]]))
+# print(class(list1[[2]]))
+# print(class(list1[[3]]))
+# print(list1[[1]])
+# print(list1[[2]])
+# print(list1[[3]])
+
+
+list2<-list(c(1,2,3),c("a","b","c"),c(T,F,T))
+print(list2)
+# print(list2[[1]][1])
+# print(list2[[1]][2])
+print(list2[[1]]) 
+print(list2[[2]])
+print(list2[[3]])
